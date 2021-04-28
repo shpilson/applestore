1b41a673b89aded92042b921def4669d167aa0a5
shpilson <kovalenko.jora@gmail.com>
2021-04-28 14:32:10 +0200

    fix

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 const advantage = document.querySelector('.custom_section');
 
-advantage.insertAdjacentHTML('afterbegin', `<h2 class="main">Наши преимущества<hr></h2>
-<div class="main inline">
-    <div class="column1">
-        <img class="arguments" src="https://github.com/shpilson/applestore/blob/master/images/argument1.png?raw=true" alt="первая причина - это you">
-        <h3>Многообразие</h3>
-    </div>
-    <div class="col-2">
-        <img class="arguments" src="https://github.com/shpilson/applestore/blob/master/images/argument2.png?raw=true" alt="а вторая - все твои dreams">
-        <h3>Низкие цены</h3>
-    </div>
-    <div class="col-3">
-        <img class="arguments" src="https://github.com/shpilson/applestore/blob/master/images/argument3.png?raw=true" alt="третья причина - это все твои words">
-        <h3>Гарантия качества</h3>
-    </div>
-    <div class="col-4">
-        <img class="arguments" src="https://github.com/shpilson/applestore/blob/master/images/argument4.png?raw=true" alt="четвертая причина - это false">
-        <h3>Быстрая доставка</h3>
-    </div>
+advantage.insertAdjacentHTML('afterbegin', `   <div class="container">
+<div class="row">
+<h2 class="w-100">Наши преимущества<hr></h2>
+<div class="col-md-3 col-6 text-center">
+    <img class="arguments" src="https://github.com/shpilson/applestore/blob/master/images/argument1.png?raw=true" alt="первая причина - это you">
+    <h3>Многообразие</h3>
+</div>
+<div class="col-md-3 col-6 text-center">
+    <img class="arguments" src="https://github.com/shpilson/applestore/blob/master/images/argument2.png?raw=true" alt="а вторая - все твои dreams">
+    <h3>Низкие цены</h3>
+</div>
+<div class="col-md-3 col-6 text-center">
+    <img class="arguments" src="https://github.com/shpilson/applestore/blob/master/images/argument3.png?raw=true" alt="третья причина - это все твои words">
+    <h3>Гарантия качества</h3>
+</div>
+<div class="col-md-3 col-6 text-center">
+    <img class="arguments" src="https://github.com/shpilson/applestore/blob/master/images/argument4.png?raw=true" alt="четвертая причина - это false">
+    <h3>Быстрая доставка</h3>
+</div>
+</div>
 </div>`)
