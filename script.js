2a3dc8df95e492edc2e6dfc2db1f062df5840285
shpilson <kovalenko.jora@gmail.com>
2021-04-29 17:28:11 +0200

    fix

--- script.js
+++ script.js
@@ -23,6 +23,6 @@ advantage.insertAdjacentHTML('afterbegin', `<div class="container">
 </div>`)
 
 
-const catalogue = document.querySelector('.page-section homefeatured_category');
+const catalogue = document.querySelector('.custom_banner');
 
 catalogue.insertAdjacentHTML('afterbegin', `<h2 class="section-title mb-4 mt-4">Категории товаров</h2>`)
