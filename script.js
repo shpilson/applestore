9f4d19f0b3790f15076ab3eb152d4c47ebf1063a
shpilson <kovalenko.jora@gmail.com>
2021-04-29 12:11:43 +0200

    fix

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const advantage = document.querySelector('.custom_section');
 
 advantage.insertAdjacentHTML('afterbegin', `<div class="container">
 <div class="row">
-<h2 class="section-title mb-4 mt-4 w-100"><span> Наш богатый ассортимент<hr></span></h2>
+<h2 class="section-title mb-4 mt-4 w-100"><span>Наш богатый ассортимент</span></h2>
 <div class="col-md-3 col-6 text-center">
     <img class="arguments" src="https://github.com/shpilson/applestore/blob/master/images/argument1.png?raw=true" alt="первая причина - это you">
-    <h3>Многообразие</h3>
+    <h6>Многообразие</h6>
 </div>
 <div class="col-md-3 col-6 text-center">
     <img class="arguments" src="https://github.com/shpilson/applestore/blob/master/images/argument2.png?raw=true" alt="а вторая - все твои dreams">
-    <h4>Низкие цены</h4>
+    <h6>Низкие цены</h6>
 </div>
 <div class="col-md-3 col-6 text-center">
     <img class="arguments" src="https://github.com/shpilson/applestore/blob/master/images/argument3.png?raw=true" alt="третья причина - это все твои words">
-    <h4>Гарантия качества</h4>
+    <h6>Гарантия качества</h6>
 </div>
 <div class="col-md-3 col-6 text-center">
     <img class="arguments" src="https://github.com/shpilson/applestore/blob/master/images/argument4.png?raw=true" alt="четвертая причина - это false">
-    <h4>Быстрая доставка</h4>
+    <h6>Быстрая доставка</h6>
 </div>
 </div>
 </div>`)
