25fcc5baf639177b58c5d7d725e0a75133c5a5d7
shpilson <kovalenko.jora@gmail.com>
2021-04-30 13:44:25 +0200

    fix

--- script.js
+++ script.js
@@ -38,6 +38,8 @@ apple_map.insertAdjacentHTML('afterend', `<div class="mymap_section" style="heig
     position: absolute;
     top: 0;
     left: 0;
+    filter: grayscale(.7);
+    transition:  all .3s ease;
     "></iframe>
 
 <div class="container">
