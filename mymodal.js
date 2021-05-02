b1bfb73f3ff05b62b4c8bace3c22e44590e59114
shpilson <kovalenko.jora@gmail.com>
2021-05-02 18:59:44 +0200

    update

--- mymodal.js
+++ mymodal.js
@@ -1,25 +1,4 @@
-let myModal = document.querySelector(".custom_section")
 
-  myModal.insertAdjacentHTML('afterbegin', `<div id="modal" class="modal">
-
-  <!-- Modal content -->
-  <div class="modal-content">
-    <div class="modal-header">
-      <span class="close" id='close'>&times;</span>
-      <h2>Добро пожаловать на сайт!</h2>
-    </div>
-    <div class="modal-body">
-      <p>Этот шаблон создан с целью демострации возможностей платформы.</p>
-      <p>Хотите такой же сайт? Присоединяйтесь!</p>
-    </div>
-    <div class="modal-footer">
-      <h3><a href="https://www.sellavi.com/">www.sellavi.com</a></h3>
-    </div>
-  </div>
-
-</div>
-
-<script src="https://shpilson.github.io/applestore/mymodal.js"></script>`)
 
 
 // Запуск скрипта после полной загрузки страницы
