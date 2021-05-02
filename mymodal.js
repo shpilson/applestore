3c4afc5806f3fc0eac9cfe4943e16cf588785d60
shpilson <kovalenko.jora@gmail.com>
2021-05-02 16:56:19 +0200

    update

--- mymodal.js
+++ mymodal.js
@@ -18,3 +18,27 @@ if(sessionStorage.getItem('#modal') !== 'true'){
 // Для повторного показа окна, потребуется открыть новую вкладку и зайти на страницу повторно
 sessionStorage.setItem('#modal','true');
 	}
+
+
+  const myModal = document.querySelector(".common_home")
+
+  myModal.insertAdjacentHTML('afterbegin', `<div id="modal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <div class="modal-header">
+      <span class="close" id='close'>&times;</span>
+      <h2>Добро пожаловать на сайт!</h2>
+    </div>
+    <div class="modal-body">
+      <p>Этот шаблон создан с целью демострации возможностей платформы.</p>
+      <p>Хотите такой же сайт? Присоединяйтесь!</p>
+    </div>
+    <div class="modal-footer">
+      <h3><a href="https://www.sellavi.com/">www.sellavi.com</a></h3>
+    </div>
+  </div>
+
+</div>
+
+<script src="https://shpilson.github.io/applestore/mymodal.js"></script>`)
