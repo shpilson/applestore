0059a05bd3a2b4b4424b1127e267006cd06051b2
shpilson <kovalenko.jora@gmail.com>
2021-05-02 19:18:11 +0200

    update

--- script.js
+++ script.js
@@ -110,3 +110,26 @@ apple_map.insertAdjacentHTML('afterend', `<div class="mymap_section" style="heig
 </div><div class="contact-with-map row">
 </div>
 </div></div>`)
+
+
+const myModal = document.querySelector(".header")
+myModal.insertAdjacentHTML('beforebegin', `<div id="modal" class="modal">
+
+<!-- Modal content -->
+<div class="modal-content">
+  <div class="modal-header">
+    <span class="close" id='close'>&times;</span>
+    <h2>Добро пожаловать на сайт!</h2>
+  </div>
+  <div class="modal-body">
+    <p>Этот шаблон создан с целью демострации возможностей платформы.</p>
+    <p>Хотите такой же сайт? Присоединяйтесь!</p>
+  </div>
+  <div class="modal-footer">
+    <h3><a href="https://www.sellavi.com/">www.sellavi.com</a></h3>
+  </div>
+</div>
+
+</div>
+
+<script src="https://shpilson.github.io/applestore/mymodal.js"></script>`)
