d39da1e45c790f9938460caa0587f1e22e49514e
shpilson <kovalenko.jora@gmail.com>
2021-05-02 19:16:08 +0200

    update

--- mymodal.js
+++ mymodal.js
@@ -10,29 +10,6 @@ $(document).ready(function ()
   
 });
 
-const myModal = document.querySelector(".header")
-myModal.insertAdjacentHTML('beforebegin', `<div id="modal" class="modal">
-
-<!-- Modal content -->
-<div class="modal-content">
-  <div class="modal-header">
-    <span class="close" id='close'>&times;</span>
-    <h2>Добро пожаловать на сайт!</h2>
-  </div>
-  <div class="modal-body">
-    <p>Этот шаблон создан с целью демострации возможностей платформы.</p>
-    <p>Хотите такой же сайт? Присоединяйтесь!</p>
-  </div>
-  <div class="modal-footer">
-    <h3><a href="https://www.sellavi.com/">www.sellavi.com</a></h3>
-  </div>
-</div>
-
-</div>
-
-<script src="https://shpilson.github.io/applestore/mymodal.js"></script>`)
-
-
 // Проверка сессии пользователя, первый ли он раз на странице
 // Если не в первый раз за сессию - блокируем показ всплывающего окна
 
