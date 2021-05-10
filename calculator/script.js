985386564147ab1e8e76fc01095620936b3625c7
shpilson <kovalenko.jora@gmail.com>
2021-05-10 22:04:39 +0200

    calculator

--- calculator/script.js
+++ calculator/script.js
@@ -23,7 +23,7 @@
  * optionGroup: основной блок с дополнительными услугами
  * additionalOptions: блок с дополнительными опциями
  * checkboxes: чекбоксы дополнительных опций
- * optionsWithPrice: дополнительные опции без чекбокса с ценой
+ * priceOfOptions: дополнительные опции без чекбокса с ценой
  * priceOfItem: основная цена самого товара
  * 
  */
@@ -32,15 +32,16 @@ const constants = {
     optionGroup: document.querySelector('.product-single #product.row .form-group.option-group'),
     additionalOptions: document.querySelector('.product-single #product.row .form-group.option-group > label ~ div'),
     checkboxes: document.querySelectorAll('.product-single #product.row .form-group.option-group > label ~ div > .checkbox'),
-    optionsWithPrice: document.querySelectorAll('.product-single #product.row .form-group.option-group > label ~ div > .checkbox > label.custom-control-label'),
+    priceOfOptions: document.querySelectorAll('.product-single #product.row .form-group.option-group > label ~ div > .checkbox > label.custom-control-label'),
     priceOfItem: document.querySelector('.product-single .product-price'),
   };
 
 export default constants;
+
 /**
  *
- * 2) В (optionsWithPrice) удаляем пробелы между символами.
- * 2.1) В (optionsWithPrice) находится цена в текстовом виде. Необходимо вернуть переменную только с цифрами,
+ * 2) В (priceOfOptions) удаляем пробелы между символами.
+ * 2.1) В (priceOfOptions) находится цена в текстовом виде. Необходимо вернуть переменную только с цифрами,
  * в числовом формате.
  * 
  */
