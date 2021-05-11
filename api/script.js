40157107061166c6aa8fe35747dfa69bbfa840d0
shpilson <kovalenko.jora@gmail.com>
2021-05-11 15:03:57 +0200

    update

--- api/script.js
+++ api/script.js
@@ -14,7 +14,7 @@ fetch("https://cors-anywhere.herokuapp.com/https://ru.sellavi.com/gateway/orders
     .then(data => res = data)
     .then(data => {
     	for(let i in res.response){
-      	document.querySelector(".table thead").insertAdjacentHTML('afterend', 
+      	document.querySelector('.table thead').insertAdjacentHTML('afterend', 
         `<tbody>
           <tr>
             <td>${res.response[i].order_id}</td>
