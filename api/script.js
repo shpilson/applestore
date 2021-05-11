5a4c4489e72a7b73b951be4af8af9955b1949545
shpilson <kovalenko.jora@gmail.com>
2021-05-11 14:04:49 +0200

    update

--- api/script.js
+++ api/script.js
@@ -3,13 +3,13 @@ let options = {
     mode: 'cors',
     headers: {
         'Accept': 'application/json',
-        'x-token': '3800f6-340d3e-4ce1ec-c90506-bbff41',
+        'x-token': 'a82f98-1fbf36-3027e0-4d7022-394230',
     }
 };
 
 let res;
 
-fetch("https://cors-anywhere.herokuapp.com/https://ru.sellavi.com/gateway/orders/100/0", options)
+fetch("https://cors-anywhere.herokuapp.com/corsdemo/https://ru.sellavi.com/gateway/orders/100/0", options)
     .then(res => res.json())
     .then(data => res = data)
     .then(data => {
