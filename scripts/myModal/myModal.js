50fc4a6f162c1ead3ad834cb99ff45b7a83cf1c8
shpilson <kovalenko.jora@gmail.com>
2021-05-12 13:01:28 +0200

    update

--- scripts/myModal/myModal.js
+++ scripts/myModal/myModal.js
@@ -20,4 +20,29 @@ myModal1.insertAdjacentHTML('afterend', `<div id="modal" class="modal">
 </div>`)
 }
 
+
+// Запуск скрипта после полной загрузки страницы
+$(document).ready(function MyModal()  
+{
+// Применяем класс закрытия по клику
+  $(".close").on("click", function () 
+	{
+		// Выбираем окно по элементу и закрываем после нажатия на "крестик"
+		$("#modal").css("display", "none")
+	});
+  
+});
+
+// Проверка сессии пользователя, первый ли он раз на странице
+// Если не в первый раз за сессию - блокируем показ всплывающего окна
+
+if(sessionStorage.getItem('#modal') !== 'true'){
+    $('#modal').css('display','block');
+// Если сессия первая - true, показываем всплывающее окно
+// Для повторного показа окна, потребуется открыть новую вкладку и зайти на страницу повторно
+sessionStorage.setItem('#modal','true');
+	}
+
+
 export default myModal;
+
