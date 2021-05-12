466eec1d49cd203805dd2365a8b1f5290ed337c2
shpilson <kovalenko.jora@gmail.com>
2021-05-12 15:30:47 +0200

    update

--- scripts/myModal/myModalProduct.js
+++ scripts/myModal/myModalProduct.js
@@ -1,7 +1,7 @@
 const myModal1 = document.querySelector(".footer")
 
 function myModalProduct() {
-myModal1.insertAdjacentHTML('afterend', `<div id="modal_product" class="modal_product">
+myModal1.insertAdjacentHTML('afterend', `<div id="modal" class="modal">
 
 <!-- Modal content -->
 <div class="modal-content">
