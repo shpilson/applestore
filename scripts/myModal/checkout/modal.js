812113e03cb2ae9e67b3266ab450a12ce7b13f08
shpilson <kovalenko.jora@gmail.com>
2021-05-24 22:55:50 +0200

    update

--- scripts/myModal/checkout/modal.js
+++ scripts/myModal/checkout/modal.js
@@ -6,15 +6,15 @@ checkoutSelector.insertAdjacentHTML('beforebegin',
 `<div id="modalCheckout" class="modalCheckout">
 
 <!-- Modal content -->
-<div class="modal-content">
-  <div class="modal-header">
-    <span class="close" id='close'>&times;</span>
+<div class="modal-content1">
+  <div class="modal-header1">
+    <span class="close1" id='close1'>&times;</span>
     <h4>Добро пожаловать в корзину!</h4>
   </div>
-  <div class="modal-body">
+  <div class="modal-body1">
     <h6>Вы находитесь на странице оформления заказа.</h6>
   </div>
-  <div class="modal-footer">
+  <div class="modal-footer1">
     <a href="https://www.sellavi.com/"><img src="https://www.sellavi.com/assets/logo.svg"></a>
   </div>
 </div>
