5c0875b0faee03b8705e98cbafbad732a631baba
shpilson <kovalenko.jora@gmail.com>
2021-05-25 16:26:27 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,9 @@ import apple_map from './apple_map/apple_map.js'; // Карта контакто
 
 // Всплывающие окна
 import myModal from './myModal/main/myModal.js'; // Модальное окно (на главной)
-import repeatMainModal from './myModal/main/myModalActivate.js'; // Модальное окно (на главной)
+import repeatMainModal from './myModal/main/myModalActivate.js'; //  Активирация окна на главной странице
 
-import myModalProduct from './myModal/product/myModalProduct.js'; // Модальное окно (на странице товара)
+// import myModalProduct from './myModal/product/myModalProduct.js'; // Модальное окно (на странице товара)
 
 import modalCheckout from './myModal/checkout/modal.js'; // Модальное окно (корзина)
 import repeatModal from './myModal/checkout/modalActivate.js'; // Активирация окна на странице корзины
