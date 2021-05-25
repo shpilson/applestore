fcfbf82b9a021d4d029154a4e7cf99e8c1bdf5c9
shpilson <kovalenko.jora@gmail.com>
2021-05-25 13:33:06 +0200

    update

--- scripts/myModal/checkout/modal.js
+++ scripts/myModal/checkout/modal.js
@@ -7,12 +7,12 @@ checkoutSelector.insertAdjacentHTML('afterend',
 <div id="woofirstpurchase-popup-container-first" style="transform: scale(0.5); bottom: -5%;">
     <form id="woofirstpurchase-main-popup-form" method="post" action="">
         <div id="woofirstpurchase-popup-first-top" style="transform: translateY(0px) rotate(1800deg);"></div>
-        <div id="woofirstpurchase-popup-first-top-text" class="woofirstpurchase-popup-disappear-group"> Sign up to get</div>
-        <div id="woofirstpurchase-popup-first-amount" class="woofirstpurchase-popup-disappear-group"> 20%</div>
-        <div id="woofirstpurchase-popup-first-bottom-text" class="woofirstpurchase-popup-disappear-group"> off your first order</div> 
+        <div id="woofirstpurchase-popup-first-top-text" class="woofirstpurchase-popup-disappear-group">Ваш персональный купон</div>
+        <div id="woofirstpurchase-popup-first-amount" class="woofirstpurchase-popup-disappear-group">10%</div>
+        <div id="woofirstpurchase-popup-first-bottom-text" class="woofirstpurchase-popup-disappear-group">скидка на первый заказ</div> 
         <input type="email" name="woofirstpurchaseemail" id="woofirstpurchase-popup-first-email-input" class="woofirstpurchase-popup-disappear-group" placeholder="Enter your mail here" required=""> 
-        <button type="submit" id="woofirstpurchase-popup-first-button" class="woofirstpurchase-popup-disappear-group"> GET IT </button>
-        <div id="woofirstpurchase-popup-first-nothanks-text" class="woofirstpurchase-popup-disappear-group">NO, THANKS</div>
+        <button type="submit" id="woofirstpurchase-popup-first-button" class="woofirstpurchase-popup-disappear-group">Спасибо!</button>
+        <div id="woofirstpurchase-popup-first-nothanks-text" class="woofirstpurchase-popup-disappear-group">Закрыть окно</div>
     </form>
 </div>
 </div>`)
