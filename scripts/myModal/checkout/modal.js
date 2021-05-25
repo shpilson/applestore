bc50d4bfa3a2feb58a37f142ac228400c7edffa3
shpilson <kovalenko.jora@gmail.com>
2021-05-25 13:29:44 +0200

    update

--- scripts/myModal/checkout/modal.js
+++ scripts/myModal/checkout/modal.js
@@ -6,7 +6,7 @@ checkoutSelector.insertAdjacentHTML('afterend',
 `<div id="woofirstpurchase-popup-background" class="woofirstpurchase-popup-background-transparent-black" style="display: flex;opacity: 1;">
 <div id="woofirstpurchase-popup-container-first" style="transform: scale(0.5); bottom: -5%;">
     <form id="woofirstpurchase-main-popup-form" method="post" action="">
-        <div id="woofirstpurchase-popup-first-top"></div>
+        <div id="woofirstpurchase-popup-first-top" style="transform: translateY(-3500px) rotate(0deg);"></div>
         <div id="woofirstpurchase-popup-first-top-text" class="woofirstpurchase-popup-disappear-group"> Sign up to get</div>
         <div id="woofirstpurchase-popup-first-amount" class="woofirstpurchase-popup-disappear-group"> 20%</div>
         <div id="woofirstpurchase-popup-first-bottom-text" class="woofirstpurchase-popup-disappear-group"> off your first order</div> 
