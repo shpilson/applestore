9aaf663e888933ee0a3b98ba6fdf9929283c60af
shpilson <kovalenko.jora@gmail.com>
2021-05-25 13:14:00 +0200

    update

--- scripts/myModal/checkout/modal.js
+++ scripts/myModal/checkout/modal.js
@@ -3,23 +3,20 @@ const checkoutSelector = document.querySelector('.checkout-checkout');
 function modalCheckout() {
 checkoutSelector.insertAdjacentHTML('afterend', 
 
-`<div id="modalCheckout" class="modalCheckout">
-
-<!-- Modal content -->
-<div class="modal-content1">
-  <div class="modal-header1">
-    <span class="close" id='close'>&times;</span>
-    <h4>Добро пожаловать в корзину!</h4>
-  </div>
-  <div class="modal-body1">
-    <h6>Вы находитесь на странице оформления заказа.</h6>
-  </div>
-  <div class="modal-footer1">
-    <a href="https://www.sellavi.com/"><img src="https://www.sellavi.com/assets/logo.svg"></a>
-  </div>
+`<div id="woofirstpurchase-popup-background" class="woofirstpurchase-popup-background-transparent-black" style="display: flex;opacity: 1;">
+<div id="woofirstpurchase-popup-container-first" style="transform: scale(0.5); bottom: -5%;">
+    <form id="woofirstpurchase-main-popup-form" method="post" action="">
+        <div id="woofirstpurchase-popup-first-top" style="transform: translateY(0px) rotate(1800deg);"></div>
+        <div id="woofirstpurchase-popup-first-top-text" class="woofirstpurchase-popup-disappear-group"> Sign up to get</div>
+        <div id="woofirstpurchase-popup-first-amount" class="woofirstpurchase-popup-disappear-group"> 20%</div>
+        <div id="woofirstpurchase-popup-first-bottom-text" class="woofirstpurchase-popup-disappear-group"> off your first order</div> 
+        <input type="email" name="woofirstpurchaseemail" id="woofirstpurchase-popup-first-email-input" class="woofirstpurchase-popup-disappear-group" placeholder="Enter your mail here" required=""> 
+        <button type="submit" id="woofirstpurchase-popup-first-button" class="woofirstpurchase-popup-disappear-group"> GET IT </button>
+        <div id="woofirstpurchase-popup-first-nothanks-text" class="woofirstpurchase-popup-disappear-group">NO, THANKS</div>
+    </form>
 </div>
-
-</div>`)
+</div>
+`)
 }
 
 export default modalCheckout;
