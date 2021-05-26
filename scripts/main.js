2aa85af51a790606a234def2e5b44a9c87f19915
shpilson <kovalenko.jora@gmail.com>
2021-05-26 23:59:59 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -14,6 +14,7 @@ import repeatModal from './myModal/checkout/modalActivate.js'; // Активир
 
  import priceNumber from './calculator/script.js'; // Базовая цена (строка) -> Число
  import inputCustom from './calculator/input.js'; // Поле для ввода кг
+ import summaryText from './calculator/input.js'; // Подсчет цены
 
 if (pages.main) {
     advantage();
@@ -28,6 +29,7 @@ if (pages.main) {
   //  myModalProduct();
     inputCustom();
     priceNumber();
+    summaryText();
   }
 
   if (pages.checkout) {
