949d08872179e40abd4a749a097c4fd8f1294594
shpilson <kovalenko.jora@gmail.com>
2021-05-26 14:02:01 +0200

    update

--- scripts/calculator/input.js
+++ scripts/calculator/input.js
@@ -9,7 +9,7 @@ function inputCustom() {
                 <input type="number" class="quantity_field form-control" name="quantity" placeholder="1" id="quantity">
 </div>
 
-<button class="btn btn-theme btn-cart btn-icon-left d-none" id="button-cart" data-loading-text="Загрузка...">
+<button class="btn btn-theme btn-cart btn-icon-left " id="button-cart" data-loading-text="Загрузка...">
 <i class="far fa-shopping-cart"></i>Купить</button>
 
 <input id="butt" type="button" value="Кнопка"/>
