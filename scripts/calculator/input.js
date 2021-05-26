ea0aa8700f8346c2a983bd1aded5b131dc01d10b
shpilson <kovalenko.jora@gmail.com>
2021-05-26 13:02:05 +0200

    update

--- scripts/calculator/input.js
+++ scripts/calculator/input.js
@@ -4,7 +4,7 @@ function inputCustom() {
     inputSelector.insertAdjacentHTML('beforebegin', `    
 <div class="quantity_new buttons_added">
     <i class="fas fa-check" style="display: none"></i>
-                <input type="number" step ="0.1" class="quantity_field form-control" name="quantity" value="1">
+                <input type="text" class="quantity_field form-control" name="quantity" value="1">
 </div>`)
 }
 
