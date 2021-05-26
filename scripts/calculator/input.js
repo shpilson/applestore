7bdc95184dc2b247b02a9e35ccc4f0c8c4406124
shpilson <kovalenko.jora@gmail.com>
2021-05-26 13:28:46 +0200

    update

--- scripts/calculator/input.js
+++ scripts/calculator/input.js
@@ -5,18 +5,8 @@ function inputCustom() {
 <div class="quantity_new buttons_added">
     <i class="fas fa-check" style="display: none"></i>
                 <input type="text" class="quantity_field form-control" name="quantity" placeholder="1" id="quantity">
-</div>
-
-<input id="butt" type="button" value="Кнопка"/><br><br>
-<div id="str"></div>
-`)
+</div>`)
 }
-
-butt.onclick = function() {
-    var val = document.getElementById('quantity').value;
-    document.getElementById('str').innerHTML="Вы ввели: "+val;
-};
-
 
 export default inputCustom;
 
