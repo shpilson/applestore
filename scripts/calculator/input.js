8b64675db15d334572665f0e0268f2d199eece65
shpilson <kovalenko.jora@gmail.com>
2021-05-27 01:27:26 +0200

    update

--- scripts/calculator/input.js
+++ scripts/calculator/input.js
@@ -14,7 +14,7 @@ function inputCustom() {
                 <input type="number" class="quantity_field form-control" name="quantity" placeholder="Количество кг" id="quantity">
 </div>
 
-<input class="btn btn-theme btn-cart btn-icon-left" id="butt" type="button" value="Купить"/>`)
+<input class="btn btn-theme btn-cart btn-icon-left" id="butt" type="button" value="Взвесить"/>`)
     
     summarySelector.insertAdjacentHTML('afterend', `
     <div class="product-availability">
