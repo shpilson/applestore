6ab9ce3827297a081792f6945245d5a3a4bf8ece
shpilson <kovalenko.jora@gmail.com>
2021-05-26 11:33:40 +0200

    update

--- scripts/calculator/script.js
+++ scripts/calculator/script.js
@@ -3,7 +3,7 @@ let priceSelector = document.querySelector('.product-price');
 let priceContent = priceSelector.textContent;
 let priceSplit = priceContent.split('.');
 let priceCut = priceSplit[0];
-let priceSplit = priceCut.split(' ').join('');
-const priceNumber = Number(priceSplit);
+let priceJoin = priceCut.split(' ').join('');
+const priceNumber = Number(priceJoin);
 
 export default priceNumber;
