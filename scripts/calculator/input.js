8c8dd54bdcfa51eb3c057d0b3afb2baf45a3358c
shpilson <kovalenko.jora@gmail.com>
2021-05-26 12:24:16 +0200

    update

--- scripts/calculator/input.js
+++ scripts/calculator/input.js
@@ -5,7 +5,7 @@ function inputCustom() {
 <div class="quantity_new buttons_added">
     <i class="fas fa-check" style="display: none"></i>
             <i data-action="minus" class="fas fa-minus quantity_key"></i>
-                <input type="number" class="quantity_field form-control" name="quantity" step="0.1" value="1">
+                <input type="number" class="quantity_field form-control" name="quantity" value="1">
             <i data-action="plus" class="fas fa-plus quantity_key"></i>
 </div>`)
 }
