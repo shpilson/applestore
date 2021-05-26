265f08e718eaf73be47871989710450bf5de8e6d
shpilson <kovalenko.jora@gmail.com>
2021-05-27 00:15:14 +0200

    update

--- scripts/calculator/input.js
+++ scripts/calculator/input.js
@@ -11,7 +11,7 @@ function inputCustom() {
     inputSelector.insertAdjacentHTML('beforebegin', `    
 <div class="quantity_new buttons_added">
     <i class="fas fa-check" style="display: none"></i>
-                <input type="number" class="quantity_field form-control" name="quantity" placeholder="1" id="quantity">
+                <input type="number" class="quantity_field form-control" name="quantity" placeholder="Количество кг" id="quantity">
 </div>
 
 <button class="btn btn-theme btn-cart btn-icon-left" id="button-cart" data-loading-text="Загрузка...">
