4e36e4139912196e18d70df310feccf08aa67b2f
shpilson <kovalenko.jora@gmail.com>
2021-05-27 00:56:39 +0200

    update

--- scripts/calculator/input.js
+++ scripts/calculator/input.js
@@ -41,7 +41,7 @@ function inputCustom() {
     document.getElementById('test').innerHTML="Вы хотите купить: " + localStorage.getItem("quantity") + " кг";
     document.getElementById('test2').innerHTML="На сумму: " + localStorage.getItem("summary") + " руб";
 
-    price.textContent = localStorage.getItem("summary");
+    price.textContent = localStorage.getItem("summary") + ".00₽";
 }
 
 export default inputCustom;
