e525b4f2571e9b365dad97bc1d108476d69acc77
shpilson <kovalenko.jora@gmail.com>
2021-05-28 20:19:52 +0200

    update

--- scripts/calculator/input.js
+++ scripts/calculator/input.js
@@ -26,53 +26,57 @@ function inputCustom() {
     let priceJoin = priceCut.split(' ').join('');
     const priceNumber = Number(priceJoin);
   
-    const calc = () => {
-      const val = document.getElementById('quantity').value;
-      const productTitle = document.querySelector('.product-title').textContent;
+        
+      
+    const openCartWrapper = () => {
+        const dropdownSelector = document.querySelector('.dropdown');
+              dropdownSelector.classList.add('show');
+  
+        const dropdownSelector2 = document.querySelector('.dropdown-menu');
+        dropdownSelector2.classList.add('show');
+    }
 
-        const summary = val * priceNumber;
-        const products = []
+    const setProductParam = () => {
+        const item = JSON.parse(localStorage.getItem("product"))
+  
+        const newpriceSelector = document.querySelector('.item-price.mt-3');
+        newpriceSelector.textContent = item.summary + '.00 ₽';
+      
+        const quantitySelector = document.querySelector('p.item-qty.mt-3');
+        quantitySelector.textContent = 'Вес: ' + item.val + ' кг';
+    
+        const newSummaryPriceSelector = document.querySelector('.row.vouchers div .media:nth-child(1) .item-price');
+        newSummaryPriceSelector.textContent = item.summary + '.00 ₽';
+    
+        const newTotalPriceSelector = document.querySelector('.row.vouchers div .media:nth-child(2) .item-price');
+        newTotalPriceSelector.textContent = item.summary + '.00 ₽';
+      }
+  
+      // Set info product
+      const calcNewValue = (param) => {
+        document.getElementById('test').innerHTML =
+          'Вы хотите купить: ' + param.val + ' кг';
+          
+        document.getElementById('test2').innerHTML =
+          'На сумму: ' + param.summary + ' руб';
+    
+        const price = document.querySelector('.product-price');
+        price.textContent = param.summary + '.00 ₽';
+    }
+    
+    const createNewProduct = () => {
+        const val = document.getElementById('quantity').value;
         const product = {
-            val, summary, productTitle
+            summary: val * priceNumber,
+            val,
+            productTitle: document.querySelector('.product-title').textContent
         }
 
       localStorage.setItem('product', JSON.stringify(product));
-  
-      document.getElementById('test').innerHTML =
-        'Вы хотите купить: ' + val + ' кг';
         
-      document.getElementById('test2').innerHTML =
-        'На сумму: ' + summary + ' руб';
-  
-      const price = document.querySelector('.product-price');
-      price.textContent = summary + '.00 ₽';
-  
-
-      
-      // open cart wrapper
-      
-      const dropdownSelector = document.querySelector('.dropdown');
-      dropdownSelector.classList.add('show');
-  
-      const dropdownSelector2 = document.querySelector('.dropdown-menu');
-      dropdownSelector2.classList.add('show');
-  
-      /* END */
-
-    /* Устанавливаем значения в корзину */
-    const item = JSON.parse(localStorage.getItem("product"))
-
-      const newpriceSelector = document.querySelector('.item-price.mt-3');
-      newpriceSelector.textContent = item.summary + '.00 ₽';
-    
-      const quantitySelector = document.querySelector('p.item-qty.mt-3');
-      quantitySelector.textContent = 'Вес: ' + item.val + ' кг';
-  
-      const newSummaryPriceSelector = document.querySelector('.row.vouchers div .media:nth-child(1) .item-price');
-      newSummaryPriceSelector.textContent = item.summary + '.00 ₽';
-  
-      const newTotalPriceSelector = document.querySelector('.row.vouchers div .media:nth-child(2) .item-price');
-      newTotalPriceSelector.textContent = item.summary + '.00 ₽';
+      openCartWrapper();
+      calcNewValue(product);
+      setProductParam();
     }
 
     butt.onclick = function () {
