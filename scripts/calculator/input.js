8aabde3ee678d8087cd7a5d648a4d572b52fbef4
shpilson <kovalenko.jora@gmail.com>
2021-05-28 16:06:51 +0200

    update

--- scripts/calculator/input.js
+++ scripts/calculator/input.js
@@ -43,7 +43,7 @@ function inputCustom() {
     const quantitySelector = document.querySelector("p.item-qty.mt-3");
     quantitySelector.textContent = "Вес: " + localStorage.getItem("quantity") + " кг";
     
-    const newSummaryPriceSelector = document.querySelectorAll(".row.vouchers div .media .item-price");
+    const newSummaryPriceSelector = document.querySelector(".row.vouchers div .media:nth-child(1) .item-price");
     newSummaryPriceSelector.textContent = localStorage.getItem("summary") + ".00 ₽";
 
     const newTotalPriceSelector = document.querySelector(".row.vouchers div .media:nth-child(2) .item-price");
