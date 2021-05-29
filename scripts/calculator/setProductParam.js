21caec6581970a156568e6301786e32370621277
shpilson <kovalenko.jora@gmail.com>
2021-05-29 19:58:53 +0200

    update

--- scripts/calculator/setProductParam.js
+++ scripts/calculator/setProductParam.js
@@ -2,16 +2,16 @@ function setProductParam() {
     const item = JSON.parse(localStorage.getItem("product"))
 
     const newpriceSelector = document.querySelector('.item-price.mt-3');
-    newpriceSelector.textContent = item.summary + '.00 ₽';
+    newpriceSelector.textContent = item.summary.toFixed(0) + '.00 ₽';
   
     const quantitySelector = document.querySelector('p.item-qty.mt-3');
     quantitySelector.textContent = 'Вес: ' + item.val + ' кг';
 
     const newSummaryPriceSelector = document.querySelector('.row.vouchers div .media:nth-child(1) .item-price');
-    newSummaryPriceSelector.textContent = item.summary + '.00 ₽';
+    newSummaryPriceSelector.textContent = item.summary.toFixed(0) + '.00 ₽';
 
     const newTotalPriceSelector = document.querySelector('.row.vouchers div .media:nth-child(2) .item-price');
-    newTotalPriceSelector.textContent = item.summary + '.00 ₽';
+    newTotalPriceSelector.textContent = item.summary.toFixed(0) + '.00 ₽';
     }
 
 export default setProductParam;
