a89e9f551679428f8fa8b08224e637bb9cce5e7f
shpilson <kovalenko.jora@gmail.com>
2021-05-30 19:39:21 +0200

    update

--- scripts/calculator/input.js
+++ scripts/calculator/input.js
@@ -55,6 +55,9 @@ function inputCustom() {
 // если есть в локале val, то суммируй с текущим значением полученным из инпута
  /* v - значение из ипута */
     const hasProdValue = (v) => {
+        if (product.val == 0) {
+            console.log("Нет предыдущего значения")
+        }
       const product = JSON.parse(localStorage.getItem('product'));
 
       const input = +(+v).toFixed(2);
