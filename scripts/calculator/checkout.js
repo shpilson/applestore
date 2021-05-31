8750842146893c13608006f8b81e684784aaaa16
shpilson <kovalenko.jora@gmail.com>
2021-05-31 12:58:48 +0200

    update

--- scripts/calculator/checkout.js
+++ scripts/calculator/checkout.js
@@ -4,7 +4,7 @@ function checkoutChange() {
     const product = JSON.parse(localStorage.getItem('product'));
     const itemPriceSelector = document.querySelector('.text-right');
     const newPrice = itemPriceSelector.textContent;
-    newPrice = "Цена: " + product.summary;
+    console.log(newPrice);
 }
 
 export default checkoutChange;
