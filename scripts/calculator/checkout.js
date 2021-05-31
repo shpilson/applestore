d330b9aad1f1b5e063524ee5a39331acbb780273
shpilson <kovalenko.jora@gmail.com>
2021-05-31 11:56:36 +0200

    update

--- scripts/calculator/checkout.js
+++ scripts/calculator/checkout.js
@@ -1,7 +1,6 @@
 import inputCustom from './input.js'; // подгружаем основной скрипт
 
 function checkoutChange() {
-    createNewProduct();
     const product = JSON.parse(localStorage.getItem('product'));
     const itemPriceSelector = document.querySelector('.single-price div');
     itemPriceSelector.textContent = "Цена: " + product.summary;
