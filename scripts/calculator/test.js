179c3cf3ce8f9eb47cfa0948b2dbd93e240b7b1c
shpilson <kovalenko.jora@gmail.com>
2021-05-31 21:03:49 +0200

    update

--- scripts/calculator/test.js
+++
@@ -1,7 +0,0 @@
-    butt.onclick = function() {
-    const val = document.getElementById('quantity').value;
-    document.getElementById('str').innerHTML="Вывод: " + (val * priceNumber);
-    }
-
-
-    
