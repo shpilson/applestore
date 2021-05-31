80db0eebb8c6284148253eab4f8a75d34d48498a
shpilson <kovalenko.jora@gmail.com>
2021-05-31 13:55:10 +0200

    update

--- scripts/calculator/checkout.js
+++ scripts/calculator/checkout.js
@@ -1,8 +1,11 @@
 function checkoutChange() {
     const product = JSON.parse(localStorage.getItem('product'));
     console.log(product);
-    const totalTableSum = document.querySelector('.total_table__sum');
-    console.log(totalTableSum);
+
+    setTimeout(() => {
+        const totalTableSum = document.querySelector('.total_table__sum');
+        console.log(totalTableSum);
+      }, 1000)
     }
 
 
