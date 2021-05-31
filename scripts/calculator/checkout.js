6b19693f92c912dd9329055ec673f90a6ab5e1ed
shpilson <kovalenko.jora@gmail.com>
2021-05-31 14:52:10 +0200

    update

--- scripts/calculator/checkout.js
+++ scripts/calculator/checkout.js
@@ -2,6 +2,7 @@ function checkoutChange() {
     const product = JSON.parse(localStorage.getItem('product'));
 
     setTimeout(() => {
+        // Меняем цену
         const totalTableSum = document.querySelector('.total_table__sum');
         totalTableSum.textContent = product.summary + ".00₽";
 
@@ -10,6 +11,16 @@ function checkoutChange() {
 
         const singlePrice = document.querySelector('.single-price div');
         singlePrice.textContent = product.summary + ".00₽";
+
+        // Добавляем значение веса
+        const itemWeight = document.querySelector(".product__description span");
+        itemWeight.insertAdjacentHTML(
+            'afterend', `<span id="newWeight"></span>`
+        );
+
+        document.getElementById('newWeight').innerHTML =
+        'Вы положили в корзину: ' + product.val + ' кг';
+
 
       }, 1000)
     }
