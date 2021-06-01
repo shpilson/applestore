8c7c5ced0c8de11ca83dddcccf2e768003456a5a
shpilson <kovalenko.jora@gmail.com>
2021-06-02 01:29:22 +0200

    update

--- scripts/calculator/input.js
+++ scripts/calculator/input.js
@@ -151,6 +151,7 @@ function inputCustom() {
     if (localStorage.getItem('product')) {
         calcNewValue(JSON.parse(localStorage.getItem('product')));
         setProductParam();
+        addToListOfProducts();
         removeButton();
     }
 
