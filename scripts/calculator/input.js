42245b3122d585f190e2da331ef51a93604b9ab0
shpilson <kovalenko.jora@gmail.com>
2021-06-01 15:34:27 +0200

    update

--- scripts/calculator/input.js
+++ scripts/calculator/input.js
@@ -117,7 +117,7 @@ function inputCustom() {
       const product = {
           summary: (val * priceNumber).toFixed(0),
           totalSummary: hasProdValue(val * priceNumber).toFixed(0),
-          newVal: hasProdValue(val),
+          newVal: hasProdValue(val) + val,
           val, // val = number
           productTitle: document.querySelector('.product-title').textContent
       }
